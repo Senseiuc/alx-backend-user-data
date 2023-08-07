93a34b2c385b863a390ecaf85ae8b5807b512518
senseiuc <dhinhojboy@gmail.com>
2023-08-07 21:20:45 +0000

    unauthorised

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorised() -> str:
+    """
+    Raises unauthorised acccess
+    """
+    abort(401)
