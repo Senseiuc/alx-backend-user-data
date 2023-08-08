476e1469ba2cce4b8732358fcfa56f6a03537683
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-08 07:16:59 +0100

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,5 +2,21 @@
 """
 Auth Class: Task 3
 """
+from typing import List, TypeVar
+
 from flask import request
-class 
+
+
+class Auth:
+    """
+    a class to manage the API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
