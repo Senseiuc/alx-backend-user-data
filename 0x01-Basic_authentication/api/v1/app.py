e4eea7fb21ab7a1cd4d33fca77cb7314a9300845
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-08 09:33:24 +0100

    auth class - before request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,12 +6,17 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', None)
+if auth_type == 'auth':
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -19,6 +24,21 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.before_request
+def handle_before_request():
+    """
+    Handles_before_request
+    :return: None
+    """
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is not None:
+        if auth.require_auth(request.path, paths) is not None:
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(401)
