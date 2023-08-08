95d931498e1048e1e4852e73b245818756f8535f
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-08 07:23:42 +0100

    auth class - added comment

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,10 +13,27 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if the required auth is given
+        :param path:
+        :param excluded_paths:
+        :return:
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Checks for authorisation header
+        :param request:
+        :return:
+        """
+
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Gets the current user
+        :param request:
+        :return:
+        """
         return None
