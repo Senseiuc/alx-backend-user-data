5a31e79a3b6f1db1b6d3bae95c61a6a9cbdf5be6
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-09 14:31:31 +0100

    rewrote the view one user function to handle - me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,7 +4,6 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-from api.v1.app import auth
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
