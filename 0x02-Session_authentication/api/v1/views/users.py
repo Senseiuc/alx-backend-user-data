06ea9c6ec86f9f98614dfa35104a2c5f66d05775
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-09 14:37:00 +0100

    rewrote the view one user function to handle - me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,9 +27,9 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is None:
         abort(404)
     if user_id == 'me':
-        if request.current_user is None:
+        if request is None:
             abort(404)
-        return jsonify(request.current_user.to_json())
+        return jsonify(request)
     user = User.get(user_id)
     if user is None:
         abort(404)
