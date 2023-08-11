9edec1f6d5f210b668329fab052b5b580cae595d
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-11 04:56:52 +0100

    app.py - added session login path and checked for cookie in the handle before request function

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,11 +38,13 @@ def handle_before_request():
     Handles_before_request
     :return: None
     """
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, paths):
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None\
+                    and auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
