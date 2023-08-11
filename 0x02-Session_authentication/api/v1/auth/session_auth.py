44953a43d3ed1ec3f8ed2a9160fec22b7d4e3781
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-11 04:12:52 +0100

    basic auth - added create session function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 SessionAuth: Task 1
 """
+from uuid import uuid4
 from .auth import Auth
 
 
@@ -9,4 +10,16 @@ class SessionAuth(Auth):
     """
     A class that authenticates using session
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session for a user
+        :param user_id: the id of the user
+        :return: the session id of the user
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+
+        self.user_id_by_session_id[str(uuid4)] = user_id
+        return user_id
