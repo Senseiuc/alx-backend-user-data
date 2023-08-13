5e8b90b90ed09535eecd74c18006ab2cfcbeb974
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-08-13 09:03:59 +0100

    session expire auth- test

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,14 +44,12 @@ def handle_before_request():
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
              '/api/v1/auth_session/login/']
     if auth:
+        print(auth)
         if auth.require_auth(request.path, paths):
             user = auth.current_user(request)
-            print(user)
             if auth.authorization_header(request) is None\
                     and auth.session_cookie(request) is None:
                 abort(401)
-            print(request)
-            abort(404)
             if user is None:
                 abort(403)
             request.current_user = user
