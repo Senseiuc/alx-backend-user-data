fbb7c2d1fe27561211386728e345e79a1ec89d38
senseiuc <dhinhojboy@gmail.com>
2023-08-16 01:01:06 +0000

    pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
