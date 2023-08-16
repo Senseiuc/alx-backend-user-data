16d78588e3fb9fdef219e1b9d8e863e7fa92dcf2
senseiuc <dhinhojboy@gmail.com>
2023-08-16 00:46:51 +0000

    readme

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+User Model: Task 0
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(200), nullable=False)
